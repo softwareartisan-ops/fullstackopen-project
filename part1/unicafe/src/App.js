a4185ca86bca47020923ccf1bf7480ae05caccbf
man.vilfer <manvilfer@gmail.com>
2023-07-12 00:05:06 +0200

    Exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,41 +1,70 @@
 import { useState } from 'react'
 
+
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [feedbackCounter, setAll] = useState(0)
+  const total = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * (-1)) / total
+  const positive = good * 100 / total
 
-
-
-  const Statistics = () => {
-    //Calculate the statistics
-    let total = good + neutral + bad
-    let average = (good * 1 + neutral * 0 + bad * (-1)) / total
-    let posPercentage = good * 100 / total
-    return <div> 
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all  {total} </p>
-            <p>average {average} </p>
-            <p>SWpositive {posPercentage} %</p>
-            </div>
+//Handler functions that increment the count when clicking button
+  const handleGood = () => {
+    setGood (good + 1)
+    setAll (feedbackCounter + 1)
   }
+  const handleNeutral = () =>{ 
+    setNeutral(neutral + 1) 
+    setAll (feedbackCounter + 1)
+  }
+  
+  const handleBad = () =>{
+    setBad(bad + 1)
+    setAll (feedbackCounter + 1)
+  }
+
 
 
   return (
     <div>
       <h1>give feedback</h1>
 
-        <button onClick={ () => setGood(good +1)}>good</button>
-        <button onClick={ () => setNeutral(neutral +1)}>neutral</button>
-        <button onClick={ () => setBad(bad +1)}>bad</button> 
-      <Statistics />
-      
+        <button onClick={handleGood}>good</button>
+        <button onClick={handleNeutral}>neutral</button>
+        <button onClick={handleBad}>bad</button> 
+ 
+      <Statistics feedbackCounter={feedbackCounter} good = {good} neutral={neutral} bad={bad} total={total} average={average} positive={positive} />
     </div>
+
+    
   )
+}
+
+const Statistics = (props) => { 
+  console.log(props)
+  if (props.feedbackCounter === 0) 
+  {    
+    return (
+      <div>
+      No Feedback given
+      </div>)
+  }  
+  return (
+   <div> 
+    <h1>statistics</h1>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+    <p>all {props.total} </p>
+    <p>average {props.average} </p>
+    <p>positive {props.positive} %</p>
+    </div>
+    )
 }
 
 export default App
